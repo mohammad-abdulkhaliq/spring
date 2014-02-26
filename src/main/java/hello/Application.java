2842e16d360dab8ea7af1ad70204de3da24a86aa
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2014-02-26 20:47:23 +0200

    mysql jdbc spring init commit

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,14 +1,53 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import org.apache.commons.dbcp.BasicDataSource;
 
-@ComponentScan
-@EnableAutoConfiguration
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String args[]) {
+        // simple DS for test (not for production!)
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/hibernateschema");
+        dataSource.setPassword("");
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        System.out.println("Creating tables");
+        jdbcTemplate.execute("drop table customers if exists");
+        jdbcTemplate.execute("create table customers(" +
+			     "id serial, first_name varchar(255), last_name varchar(255))");
+
+        String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
+        for (String fullname : names) {
+            String[] name = fullname.split(" ");
+            System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
+            jdbcTemplate.update(
+				"INSERT INTO customers(first_name,last_name) values(?,?)",
+				name[0], name[1]);
+        }
+
+        System.out.println("Querying for customer records where first_name = 'Josh':");
+        List<Customer> results = jdbcTemplate.query(
+						    "select * from customers where first_name = ?", new Object[] { "Josh" },
+						    new RowMapper<Customer>() {
+                    @Override
+                    public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        return new Customer(rs.getLong("id"), rs.getString("first_name"),
+					    rs.getString("last_name"));
+                    }
+						    });
+
+        for (Customer customer : results) {
+            System.out.println(customer);
+        }
     }
 }
