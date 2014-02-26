61d1701cac091ce18ef52f79f28ec3ec76f9e0c4
mohammad-abdulkhaliq <mohammad28march1993@gmail.com>
2014-02-26 20:58:15 +0200

    Working branch

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -22,22 +22,22 @@ public class Application {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
         System.out.println("Creating tables");
-        jdbcTemplate.execute("drop table customers if exists");
+        jdbcTemplate.execute("drop table if exists customers;");
         jdbcTemplate.execute("create table customers(" +
-			     "id serial, first_name varchar(255), last_name varchar(255))");
+			     "id serial, first_name varchar(255), last_name varchar(255));");
 
         String[] names = "John Woo;Jeff Dean;Josh Bloch;Josh Long".split(";");
         for (String fullname : names) {
             String[] name = fullname.split(" ");
             System.out.printf("Inserting customer record for %s %s\n", name[0], name[1]);
             jdbcTemplate.update(
-				"INSERT INTO customers(first_name,last_name) values(?,?)",
+				"INSERT INTO customers(first_name,last_name) values(?,?);",
 				name[0], name[1]);
         }
 
         System.out.println("Querying for customer records where first_name = 'Josh':");
         List<Customer> results = jdbcTemplate.query(
-						    "select * from customers where first_name = ?", new Object[] { "Josh" },
+						    "select * from customers where first_name = ?;", new Object[] { "Josh" },
 						    new RowMapper<Customer>() {
                     @Override
                     public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
